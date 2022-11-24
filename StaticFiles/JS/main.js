45b0082d19e7dcb38c51d8c149ae37bfbd3471d7
YuriiFridman <102798350+YuriiFridman@users.noreply.github.com>
2022-11-24 23:04:09 +0200

    1.1

--- StaticFiles/JS/main.js
+++ StaticFiles/JS/main.js
@@ -4,8 +4,8 @@ const navBtnImg = document.querySelector('#nav-btn-img');
 
 navBtn.onclick = () => {
     if (nav.classList.toggle('open')) {
-        navBtnImg.src = "StaticFiles/IMG/nav-close.svg";
+        navBtnImg.src = "../StaticFiles/IMG/nav-close.svg";
     } else {
-        navBtnImg.src = "StaticFiles/IMG/nav-open.svg";
+        navBtnImg.src = "../StaticFiles/IMG/nav-open.svg";
     }
 }
